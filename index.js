30d76e922349c7a7a925ab31cb38ed54427ca3e7
Jordan M <jormmiller@gmail.com>
2024-04-30 13:40:28 -0400

    implemented inquirer functionality in index.js

--- index.js
+++ index.js
@@ -31,6 +31,70 @@ Delete departments, roles, and employees.
 
 View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
 */
+function init() {
+    console.log('Welcome to Employee Manager!');
+    displayMainPrompt();
+}
+
+
+function displayMainPrompt() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Update employee manager', 'View employees by manager', 'View employees by department', 'Delete department', 'Delete role', 'Delete employee', 'View department budgets', 'Quit']
+        }
+    ]).then(response => {
+        switch (response.option) {
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Update employee manager':
+                updateEmployeeManager();
+                break;
+            case 'View employees by manager':
+                viewEmployeesByManager();
+                break;
+            case 'View employees by department':
+                viewEmployeesByDepartment();
+                break;
+            case 'Delete department':
+                deleteDepartment();
+                break;
+            case 'Delete role':
+                deleteRole();
+                break;
+            case 'Delete employee':
+                deleteEmployee();
+                break;
+            case 'View department budgets':
+                viewDepartmentBudgets();
+                break;
+            case 'Quit':
+                quit();
+        }
+    });
+
+}
 
 /*
 
@@ -53,13 +117,222 @@ close() - closes the connection
 */
 
 function viewAllDepartments() {
-    db.viewAllDepartments()
-        .then(([rows]) => {
-            let departments = rows;
-            console.log('\n');
-            console.table(departments);
-        })
-        .catch(err => console.log(err))
+    db.viewAllDepartments().then(([rows]) => {
+        console.table(rows);
+        displayMainPrompt();
+    });
 }
 
-viewAllDepartments();
+function viewAllRoles() {
+    db.viewAllRoles().then(([rows]) => {
+        console.table(rows);
+        displayMainPrompt();
+    });
+}
+
+function viewAllEmployees() {
+    db.viewAllEmployees().then(([rows]) => {
+        console.table(rows);
+        displayMainPrompt();
+    });
+}
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the name of the department:'
+        }
+    ]).then(response => {
+        db.addDepartment(response).then(() => {
+            console.log('Department added successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of the role:'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter the salary of the role:'
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'Enter the department id of the role:'
+        }
+    ]).then(response => {
+        db.addRole(response).then(() => {
+            console.log('Role added successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter the first name of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter the last name of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'Enter the role id of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'Enter the manager id of the employee:'
+        }
+    ]).then(response => {
+        db.addEmployee(response).then(() => {
+            console.log('Employee added successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function updateEmployeeRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter the id of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'Enter the id of the new role:'
+        }
+    ]).then(response => {
+        db.updateEmployeeRole(response.employeeId, response.roleId).then(() => {
+            console.log('Employee role updated successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function updateEmployeeManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter the id of the employee:'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter the id of the new manager:'
+        }
+    ]).then(response => {
+        db.updateEmployeeManager(response.employeeId, response.managerId).then(() => {
+            console.log('Employee manager updated successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function viewEmployeesByManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter the id of the manager:'
+        }
+    ]).then(response => {
+        db.viewEmployeesByManager(response.managerId).then(([rows]) => {
+            console.table(rows);
+            displayMainPrompt();
+        });
+    });
+}
+
+function viewEmployeesByDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentId',
+            message: 'Enter the id of the department:'
+        }
+    ]).then(response => {
+        db.viewEmployeesByDepartment(response.departmentId).then(([rows]) => {
+            console.table(rows);
+            displayMainPrompt();
+        });
+    });
+}
+
+function deleteDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentId',
+            message: 'Enter the id of the department:'
+        }
+    ]).then(response => {
+        db.deleteDepartment(response.departmentId).then(() => {
+            console.log('Department deleted successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function deleteRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'Enter the id of the role:'
+        }
+    ]).then(response => {
+        db.deleteRole(response.roleId).then(() => {
+            console.log('Role deleted successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function deleteEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter the id of the employee:'
+        }
+    ]).then(response => {
+        db.deleteEmployee(response.employeeId).then(() => {
+            console.log('Employee deleted successfully!');
+            displayMainPrompt();
+        });
+    });
+}
+
+function viewDepartmentBudgets() {
+    db.viewDepartmentBudgets().then(([rows]) => {
+        console.table(rows);
+        displayMainPrompt();
+    });
+}
+
+function quit() {
+    db.close();
+    console.log('Goodbye!');
+    process.exit();
+}
+
+init();
